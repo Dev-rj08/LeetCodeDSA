907f217f2c137cf8c9728815e99e9360097c2a7a
Dev-rj08 <dev.rj.vlr@gmail.com>
2024-09-01 19:04:38 +0530

    20.java added;2A

---
+++ 20.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isValid(String s) {
+        while(true){
+            if(s.contains("{}")){
+                s=s.replace("{}","");
+            }
+            else if(s.contains("[]")){
+                s=s.replace("[]","");
+            }
+            else if(s.contains("()")){
+                s=s.replace("()","");
+            }
+            else{
+                return s.isEmpty();
+            }
+        }
+    }
+}
